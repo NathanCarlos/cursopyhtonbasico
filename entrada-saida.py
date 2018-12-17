7e9a20c385ad1438a9e54e477879947a459ac323
nathancarlos12 <nathan.lima@fcamara.com.br>
2018-12-17 10:58:49 -0200

    Exercícios iniciais

---
+++ entrada-saida.py
@@ -0,0 +1,11 @@
+#Tipando input.
+idade = int(input('Qual sua idade? '))
+nome = str(input('Qual seu nome?'))
+altura = float(input('Qual sua altura?'))
+frase = nome + ' tem ' + str(idade) + ' anos ' + 'e ' + str(altura) + ' de altura'
+
+print(nome, 'tem', idade, 'anos', 'e', altura, 'de altura.')
+
+'''
+Tipando a entrada de dados e exibindo resposta transformando em string.
+'''

f333b65599487db24d41e8decb2b57d90c560e70
nathancarlos12 <nathan.lima@fcamara.com.br>
2018-12-17 11:55:48 -0200

    Aula 3 Solyd

---
+++ exercicio-solyd.py
@@ -0,0 +1,8 @@
+idade = int(input('Digite sua idade: '))
+peso = float(input('Digite seu peso: '))
+altura = float(input('Digite sua altura: '))
+
+if idade >= 18 and peso >= 60 and altura >= 1.70:
+    print('Você está apto a entrar no exército!')
+else:
+    print('Você não está apto a entrar no exército!')
---
+++ operadores-logicos.py
@@ -0,0 +1,30 @@
+'''
+Comparações: ==, !=, >, >=, <=
+Comparações: and, or
+'''
+# var_true = True
+# var_false = False
+# n1 = float(input('Digite um número: '))
+# n2 = float(input('Digite outro número: '))
+#
+# if n1 > n2:
+#     print(n1,'é maior que',n2)
+# else:
+#     print(n2, 'é maior que', n1)
+# print('Opções:\n1 = Escreve Nathan\n2 = Escreve João\n3 = Escreve Maria')
+# opcao = int(input('Escolha uma opção: '))
+#
+# #Else if no python é elif.
+# if opcao == 1:
+#     print('Nathan')
+# elif opcao == 2:
+#     print('João')
+# elif opcao == 3:
+#     print('Maria')
+# else:
+#     print('Opção inválida!')
+# idade = 50
+# if not idade == 50:
+#     print('Você não tem', idade, 'Anos')
+# else:
+#     print('Você tem', idade, 'Anos')

bb6d2335cc098a461b63afa776f1f6dbad9cdf5c
nathancarlos12 <nathan.lima@fcamara.com.br>
2018-12-17 13:26:54 -0200

    Aula 7 pytho

---
+++ aula7-funcoes-metodos.py
@@ -0,0 +1,6 @@
+#Para definir uma funcão colocamos def.
+def soma(n1, n2):
+    result = n1 + n2
+    return result
+
+print(soma(input('Digite o primeiro número: '),input('Digite o segundo número: ')))
